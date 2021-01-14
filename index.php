bd7d671cfce7422410ed54305d24667ac6dddb34
rarohling <65776895+rarohling@users.noreply.github.com>
2021-01-14 10:50:54 -0400

    Links Quebrados
    Correção dos Links

--- index.php
+++ index.php
@@ -11,10 +11,10 @@
 <center><h1>Drinks</h1></center>
 
 <div style="align-item<br> s: center;display: flex;flex-direction: row;flex-wrap: wrap;justify-content: center;">
-    <a href="http://mabilete.com.br/projeto/apis/categories/categories.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">categories</div></div></a>
-    <a href="http://mabilete.com.br/projeto/apis/glasses/glasses.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">glasses</div></div></a>
-    <a href="http://mabilete.com.br/projeto/apis/ingredients/ingredients.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">ingredients</div></div></a>
-    <a href="http://mabilete.com.br/projeto/apis/alcoholic/alcoholic.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">alcoholic</div></div></a>
+    <a href="apis/categories/categories.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">categories</div></div></a>
+    <a href="apis/glasses/glasses.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">glasses</div></div></a>
+    <a href="apis/ingredients/ingredients.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">ingredients</div></div></a>
+    <a href="apis/alcoholic/alcoholic.php"><div class="zoom" style="float: left; width: 200px; height: 200px; background: #dddddd; padding:4px;border-style: solid;border-color: #ffffff;"><div class="label">alcoholic</div></div></a>
 </div>
 
 </body>
